a86ef1ffafe03fa43ebacfc7ba1f8e12a9a69490
Euro <dimaseuro0706@gmail.com>
2026-01-23 00:15:25 +0700

    Auth -api

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['admin','staf','penghuni']);
-            $table->rememberToken();
+            $table->enum('role',['owner','staf','penghuni']);
             $table->timestamps();
         });
 
